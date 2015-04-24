9743cb50d9c170703af2ab57b6c8e8a1ddf03826
嘿'''大苹果 <853049349@qq.com>
2015-04-24 10:02:53 +0800

    SwingCopters
    小游戏

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void OnMouseDown()
+    {
+        audio.Play();
+        Vector3 palyerScreenPosition = Camera.main.WorldToScreenPoint(player.transform.position);
+        float dx = Input.mousePosition.x - palyerScreenPosition.x;
+        float x = dx > 0 ? 5 : -5;
+        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(x, 120));
+        if (dx > 0) player.GetComponent<Player>().TurnRight();
+        else player.GetComponent<Player>().TurnLeft();
+    }
+	
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Transform body;
+    public void TurnRight() {
+        body.localScale = new Vector3(1, 1, 1);
+    }
+    public void TurnLeft()
+    {
+        body.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Hammer") {
+            print("effffffff");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Hammer")
+        {
+            audio.Play();
+        }
+    }
+
+
+}
